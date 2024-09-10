33039843277350141c63b973c43bec2ab7fa2ff9
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-09-10 20:21:02 +0530

    change allowhosts

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost 127.0.0.1 [::1] weatherstation-server.onrender.com").split(" ")
 
 SITE_ID = 1
 
