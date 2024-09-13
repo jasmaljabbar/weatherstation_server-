99b30af59198d8ae200771630337ef50654f2069
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-09-13 09:53:28 +0530

    change all env in to environ

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -29,12 +29,12 @@ environ.Env.read_env(str(BASE_DIR / ".env"))  # Only for local dev
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ.get('DEBUG', 'True') == "True"
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS", default="").split(",")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(",")
 
 SITE_ID = 1
 
@@ -221,10 +221,10 @@ DJOSER = {
 
 
 
-EMAIL_BACKEND =env("EMAIL_BACKEND")
-EMAIL_HOST =env("EMAIL_HOST")
-EMAIL_PORT =env("EMAIL_PORT")
-EMAIL_USE_TLS =env("EMAIL_USE_TLS")
-EMAIL_HOST_USER =env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD =env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL =env("DEFAULT_FROM_EMAIL")
+EMAIL_BACKEND =os.environ.get("EMAIL_BACKEND")
+EMAIL_HOST =os.environ.get("EMAIL_HOST")
+EMAIL_PORT =os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS =os.environ.get("EMAIL_USE_TLS")
+EMAIL_HOST_USER =os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD =os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL =os.environ.get("DEFAULT_FROM_EMAIL")
